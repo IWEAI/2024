140a8dfc7bb5cd3f20ca5cd03f580aaa009e84bf
Aslang666 <Aslang666@users.noreply.github.com>
2024-07-31 06:24:05 +0000

    deploy: 9088a3000edbc5e8e03528c73d9a29e0b2bf7197

--- assets/index-zzaSRtg0.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./TBDCard-S76bdFJP.js";import{d as a,c as o,e as t,o as n,_ as s}from"./index-_ELCemio.js";const c={class:"keynotes-container animate__animated animate__fadeIn",id:"keynotes"},_=a({__name:"index",setup(d){return(r,m)=>(n(),o("div",c,[t(e,{class:"commom"})]))}}),f=s(_,[["__scopeId","data-v-d108f4c4"]]);export{f as default};
