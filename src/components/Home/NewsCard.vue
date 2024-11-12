2ea11668a78619a8e6ff013c1e299259bb5a3be2
Aslang666 <liupeihong@primedigitaltech.com>
2024-11-12 09:23:25 +0800

    新闻内容

--- src/components/Home/NewsCard.vue
+++ src/components/Home/NewsCard.vue
@@ -9,7 +9,12 @@
                     <li>(07 August) For authors whose papers are rejected by PRICAI 2023, please re-submit to PKAW 2023 through the submission link ASAP. The submission link is open until 20 August.</li>
                     <li>(01 August) The submission deadline has been extended to 7 August.</li>
                     <li>(01 July) The submission site is open!</li> -->
-                    <li>(01 July) The website is open!</li>
+                    <li>July 01, 2024: The website is open!</li>
+                    <li>October 11, 2024: The IWEAI Program has been published on our workshop website.</li>
+                    <li>September 22, 2024. The PRICAI Program at a glance has been published on the PRICAI conference website!</li>
+                    <li>August 21, 2024: The Early Registration deadline has been extended to September 8th.</li>
+                    <li>August 19, 2024: Information about applying VISA has been added to the PRICAI REGISTRATION page.</li>
+                    <li>August 13, 2024: Notice of acceptance. Camera-Ready Submission time has been set on September 4, 2024 (AoE).</li>
                 </ul>
             </div>
         <!-- </el-card> -->
@@ -44,5 +49,9 @@ ul{
     display: flex;
     flex-direction: column;
     justify-content: center;
+}
+li{
+    padding: 10px 0;
+    /* border-bottom: 1px solid #04162c; */
 }
 </style>
